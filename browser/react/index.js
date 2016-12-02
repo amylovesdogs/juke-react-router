477109cec1fd49d9125e0a64153b5c6b99e30601
BuildTools <amypaschal@me.com>
2016-12-02 14:04:47 -0700

    stopping point

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import Albums from './components/Albums';
+import Album from './components/Album';
+import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
 
 ReactDOM.render(
-  <AppContainer />,
+  <div>
+    <Router history={browserHistory}>
+      <Route path='/' component={AppContainer}>
+        <IndexRedirect to="/albums" />
+          {/* Our child routes will go here!
+          The components that these child routes render will be the "children"
+          of the component that the parent route renders!  */}
+        <Route path="/albums/:albumId" component={Album} />
+        <Route path='/albums' component={Albums}/>
+      </Route>
+    </Router>
+  </div>,
   document.getElementById('app')
 );
